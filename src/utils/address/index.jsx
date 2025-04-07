57e32f46eee989f30727405884c4f460db7abd0a
BuildTools <unconfigured@null.spigotmc.org>
2025-04-07 17:02:40 +0800

    版本2.5.9：更新后台管理系统地址

--- src/utils/address/index.jsx
+++ src/utils/address/index.jsx
@@ -1,6 +1,6 @@
 // const cpo_address = "http://47.115.73.132:20000"
 // const cpo_address = "http://8.134.32.42:20000"
-const cpo_address = "http://houtai.chuangzaoshi.com:20000"
+const cpo_address = "http://houtai.chuangzaoshi.com"
 
 const api_proxy_address = "http://8.134.32.42:8000"
 // const api_proxy_address = "http://47.115.73.132:8000"
