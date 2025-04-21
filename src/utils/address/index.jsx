4aaa6b9613674d0acf8201c3f1eb470cb93fff96
BuildTools <unconfigured@null.spigotmc.org>
2025-04-21 22:44:55 +0800

    版本2.7.1：更新API地址

--- src/utils/address/index.jsx
+++ src/utils/address/index.jsx
@@ -4,12 +4,14 @@
 const cpo_address = "https://suanadminli.lkun.com.cn"
 
 // const api_proxy_address = "http://8.134.32.42:8000"
-const api_proxy_address = "http://1.14.192.61:8000"
+// const api_proxy_address = "https://1.14.192.61:8000"
+const api_proxy_address = "https://suanadminli.lkun.com.cn"
 // const api_proxy_address = "http://47.115.73.132:8000"
 
 
 // const api_address = "http://8.134.32.42:8000"
-const api_address = "http://1.14.192.61:8000"
+// const api_address = "https://1.14.192.61:8000"
+const api_address = "https://suanadminli.lkun.com.cn"
 // const api_address = "http://47.115.73.132:8000"
 // const api_address = ""
 
