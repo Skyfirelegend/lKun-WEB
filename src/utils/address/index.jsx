9ca747a5e48b2dc8d83a9bcd574e2495a7ed3510
BuildTools <unconfigured@null.spigotmc.org>
2025-04-21 15:01:16 +0800

    版本2.7：更新正式部署网页地址、修改客户案例字体大小

--- src/utils/address/index.jsx
+++ src/utils/address/index.jsx
@@ -1,12 +1,15 @@
 // const cpo_address = "http://47.115.73.132:20000"
 // const cpo_address = "http://8.134.32.42:20000"
-const cpo_address = "http://houtai.chuangzaoshi.com"
+// const cpo_address = "http://houtai.chuangzaoshi.com"
+const cpo_address = "https://suanadminli.lkun.com.cn"
 
-const api_proxy_address = "http://8.134.32.42:8000"
+// const api_proxy_address = "http://8.134.32.42:8000"
+const api_proxy_address = "http://1.14.192.61:8000"
 // const api_proxy_address = "http://47.115.73.132:8000"
 
 
-const api_address = "http://8.134.32.42:8000"
+// const api_address = "http://8.134.32.42:8000"
+const api_address = "http://1.14.192.61:8000"
 // const api_address = "http://47.115.73.132:8000"
 // const api_address = ""
 
